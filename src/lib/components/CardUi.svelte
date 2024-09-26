5f39e9d8d5eed7a69851c071626eea2dae5e625a
Silent Echo <silent@silentecho.eu>
2024-09-26 17:25:31 +0200

    breakout Card component

---
+++ src/lib/components/CardUi.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { Card } from "$lib/types";
+
+	let { card, onedit } = $props<{
+		card: Card;
+		onedit?: (cardId: number) => void;
+	}>();
+</script>
+
+<h2>
+	{card.question}
+</h2>
+<button
+	aria-label="edit card"
+	onclick={() => {
+		onedit?.(card.id);
+	}}
+	class="icon-solid icon-after icon-pencil"
+>
+</button>
